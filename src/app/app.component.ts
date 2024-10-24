ff60e5b1d6447c89653ddf47aa9524b6038dc9e7
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-24 20:42:37 +0300

    add reusable dashboard-item component with ng-content for article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { ServerStatusComponent } from "./components/server-status/server-status.component";
 import { TicketsComponent } from "./components/tickets/tickets.component";
 import { TrafficComponent } from "./components/traffic/traffic.component";
+import { DashboardItemComponent } from "./shared/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ServerStatusComponent, TicketsComponent, TrafficComponent],
+  imports: [RouterOutlet, HeaderComponent, ServerStatusComponent, TicketsComponent, TrafficComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
