a38264e0e6ccfcaf10452962bdc9eed7321de08f
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-25 17:06:58 +0300

    add shared button component (another usage)

--- src/app/components/tickets/new-ticket/new-ticket.component.ts
+++ src/app/components/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../../../shared/button/button.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [ButtonComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
