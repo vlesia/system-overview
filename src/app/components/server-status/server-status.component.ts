a06fd4cf05baa3a1b072e51b3332eed867c476a3
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-28 11:22:22 +0200

    add dynamic class binding and logic to change status in server status component, add status model

--- src/app/components/server-status/server-status.component.ts
+++ src/app/components/server-status/server-status.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { Status } from '../../models/status.model';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: Status = 'offline';
 
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
