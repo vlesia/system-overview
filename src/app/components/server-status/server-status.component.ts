a731d2d202c15df91fcd3497a14ca94247d2743e
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-29 15:48:39 +0200

    implement signal and function effect in serve-status component

--- src/app/components/server-status/server-status.component.ts
+++ src/app/components/server-status/server-status.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  DestroyRef,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { Status } from '../../models/status.model';
 
 @Component({
@@ -14,20 +9,24 @@ import { Status } from '../../models/status.model';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: Status = 'offline';
+  currentStatus = signal<Status>('offline');
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    // effect(() => {
+    //   console.log(this.currentStatus());
+    // });
+  }
 
   ngOnInit(): void {
     const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
